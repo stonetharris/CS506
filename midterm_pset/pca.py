fb14c2f0f865ec10e7b43b5edea422c0b32196d8
Stone Harris <stonetharris@gmail.com>
2024-03-31 12:04:28 -0400

    Did midterm problem set

--- midterm_pset/pca.py
+++ midterm_pset/pca.py
@@ -1,20 +1,39 @@
 import numpy as np
 import pandas as pd
-from numpy.random import uniform, normal
 from scipy.stats import multivariate_normal
 from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 train_data = pd.read_csv('./voweltrain.csv', index_col=0)
 test_data = pd.read_csv('./voweltest.csv', index_col=0)
-
-np.random.seed(42)
 
 X_train = train_data.filter(regex='x.*')
 y_train = train_data['y']
 X_test = test_data.filter(regex='x.*')
 y_test = test_data['y']
+
+class PCAFromScratch:
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.components = None
+        self.mean = None
+
+    def fit(self, X):
+        self.mean = np.mean(X, axis=0)
+        X = X - self.mean
+        
+        covariance_matrix = np.cov(X.T)
+
+        eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
+        eigenvectors = eigenvectors[:, np.argsort(-eigenvalues)]
+        self.components = eigenvectors[:, :self.n_components]
+
+    def transform(self, X):
+        X = X - self.mean
+        return np.dot(X, self.components)
 
 class Component:
     def __init__(self, mixture_prop, mean, covariance):
@@ -65,15 +84,50 @@ class GMMFromScratch:
         self.expectation(dataset)
         return np.argmax(self.probs, axis=1)
 
-gmm = GMMFromScratch(n_components=11, n_iterations=100)
-gmm.fit(X_train.values)
-predicted_labels = gmm.predict(X_test.values)
-print(f"Predicted labels: {np.bincount(predicted_labels)}")
-print(f"Actual labels: {np.bincount(y_test)}")
-accuracy = accuracy_score(y_test, predicted_labels)
-misclassification_rate = 1 - accuracy
-report = classification_report(y_test, predicted_labels, digits=4)
 
-print(report)
-print(f"Accuracy: {accuracy}")
-print(f"Misclassification Rate: {misclassification_rate}")
+# pca = PCAFromScratch(n_components=2)
+# pca.fit(X_train.values)
+# X_train_pca = pca.transform(X_train.values)
+# X_test_pca = pca.transform(X_test.values)
+# classifier = RandomForestClassifier()
+# classifier.fit(X_train_pca, y_train)
+# y_pred = classifier.predict(X_test_pca)
+
+# accuracy = accuracy_score(y_test, y_pred)
+# print(f'Accuracy of PCA-transformed data with RandomForestClassifier: {accuracy:.4f}')
+    
+pca = PCAFromScratch(n_components=3)
+pca.fit(X_train.values)
+
+X_train_pca = pca.transform(X_train.values)
+X_test_pca = pca.transform(X_test.values)
+
+gmm = GMMFromScratch(n_components=11, n_iterations=100)
+gmm.fit(X_train_pca)
+gmm_labels = gmm.predict(X_test_pca) + 1
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+class_to_color_map = {
+    1: "red",
+    2: "blue",
+    3: "green",
+    4: "yellow",
+    5: "orange",
+    6: "purple",
+    7: "pink",
+    8: "brown",
+    9: "gray",
+    10: "olive",
+    11: "cyan",
+}
+
+colors = [class_to_color_map[label] for label in gmm_labels]
+ax.scatter(X_test_pca[:, 0], X_test_pca[:, 1], X_test_pca[:, 2], c=colors)
+
+ax.set_xlabel('Principal Component 1')
+ax.set_ylabel('Principal Component 2')
+ax.set_zlabel('Principal Component 3')
+plt.title('3D Scatter Plot of Vowel Data (Colored by GMM Labels)')
+plt.show()
